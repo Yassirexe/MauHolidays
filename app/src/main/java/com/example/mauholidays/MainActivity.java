73e0a5aedffc73bf256926e64e25855f0dec3e33
KawsharEmanden <123965793+KawsahrEmanden@users.noreply.github.com>
2023-05-22 21:12:42 +0400

    settings
    screens for setting

--- app/src/main/java/com/example/mauholidays/MainActivity.java
+++ app/src/main/java/com/example/mauholidays/MainActivity.java
@@ -2,21 +2,26 @@ package com.example.mauholidays;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //declare variables
     EditText username, password, repassword;
-    Button signup, signin;
+    Button signup, signin, Edit;
     DBHelper DB;
+    ImageView privacy, helpsetting, about;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +33,12 @@ public class MainActivity extends AppCompatActivity {
         repassword = (EditText) findViewById(R.id.repassword);
         signup = (Button) findViewById(R.id.btnsignup);
         signin = (Button) findViewById(R.id.btnsignin);
-        DB= new DBHelper(this);
+        Edit = (Button) findViewById(R.id.buttonEdit);
+        privacy = (ImageView) findViewById(R.id.buttonPrivacy);
+        helpsetting = (ImageView) findViewById(R.id.buttonHelp);
+        about = (ImageView) findViewById(R.id.buttonAbout);
+
+        DB = new DBHelper(this);
 
         //listeners for buttons
 
@@ -42,20 +52,19 @@ public class MainActivity extends AppCompatActivity {
 
                 if (user.equals("") || pass.equals("") || repass.equals(""))
                     Toast.makeText(MainActivity.this, "please fill all the fields", Toast.LENGTH_SHORT).show();
-                else{
-                    if (pass.equals(repass)){
+                else {
+                    if (pass.equals(repass)) {
                         Boolean checkuser = DB.checkusername(user);
-                        if(checkuser == false){
+                        if (checkuser == false) {
                             Boolean insert = DB.insertdata(user, pass);
-                            if(insert == true){
+                            if (insert == true) {
                                 Toast.makeText(MainActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
-                              //  Intent intent = new Intent(getApplicationContext(), Home.class);
-                              //  startActivity(intent);
-                            }else {
+                                //  Intent intent = new Intent(getApplicationContext(), Home.class);
+                                //  startActivity(intent);
+                            } else {
                                 Toast.makeText(MainActivity.this, "Sign Up failed", Toast.LENGTH_SHORT).show();
                             }
-                        }
-                        else {
+                        } else {
                             Toast.makeText(MainActivity.this, "User already found , Please Sign in", Toast.LENGTH_SHORT).show();
                         }
                     } else {
@@ -73,31 +82,49 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        Edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, EditProfile.class);
+                startActivity(intent);
+            }
+        });
+        privacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, PrivacyPolicy.class);
+                startActivity(intent);
+            }
+        });
+        helpsetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, HelpSetting.class);
+                startActivity(intent);
+            }
+
+        });
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, about.class);
+                startActivity(intent);
+            }
+
+        });
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
