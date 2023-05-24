a3e3a78db5462de68d79a15715f6e342afe5cdb1
muntasirhasan1 <muntasir319@gmail.com>
2023-05-24 20:29:22 +0400

    stached corrected

--- app/src/main/java/com/example/mauholidays/screen345.java
+++ app/src/main/java/com/example/mauholidays/screen345.java
@@ -2,7 +2,6 @@ package com.example.mauholidays;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< Updated upstream
 import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -13,18 +12,10 @@ import android.widget.RadioButton;
 public class screen345 extends AppCompatActivity {
     private Button b1;
 
-    @SuppressLint("MissingInflatedId")
-=======
-import android.os.Bundle;
-
-public class screen345 extends AppCompatActivity {
-
->>>>>>> Stashed changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen345);
-<<<<<<< Updated upstream
         b1 = findViewById(R.id.quizsubmit);
 
         b1.setOnClickListener(new View.OnClickListener() {
@@ -121,7 +112,5 @@ public class screen345 extends AppCompatActivity {
 
         });
 
-=======
->>>>>>> Stashed changes
     }
 }
